9811baddaf0ad986c0f15c83bfb20bf47a15590b
Yurii Onats <yura.onats@gmail.com>
2021-10-29 22:43:31 +0300

    finich

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,6 +1,7 @@
 import countryTemp from '../templates/country.hbs'
 import countriesTemp from '../templates/countries.hbs'
 import clearMarkup from './cliner'
+import specificQuery from './specific'
 
 const resultTemplate = document.querySelector('.result')
 
@@ -14,52 +15,11 @@ if (country.length === 1){
     resultTemplate.insertAdjacentHTML('beforeend', countriesTemp(country))        
     return;    
 } else {
-    // showStackTopLeft('error');
-//     PNotify.error({
-//   title: 'Oh No!',
-//   text: 'Something terrible happened.'
-// });
+    clearMarkup();
+    specificQuery();    
     console.log('забагато');        
     }   
 }
 
 export { resultTemplate };
 export default createTemplate;
-
-
-// function showStackTopLeft(type) {
-//   if (typeof window.stackTopLeft === 'undefined') {
-//     window.stackTopLeft = new PNotify.Stack({
-//       dir1: 'down',
-//       dir2: 'right',
-//       firstpos1: 25,
-//       firstpos2: 25,
-//       push: 'top',
-//       maxStrategy: 'close'
-//     });
-//   }
-//   const opts = {
-//     title: 'Over Here',
-//     text: "Check me out. I'm in a different stack.",
-//     stack: window.stackTopLeft
-//   };
-//   switch (type) {
-//     case 'error':
-//       opts.title = 'Oh No';
-//       opts.text = 'Watch out for that water tower!';
-//       opts.type = 'error';
-//       break;
-//     case 'info':
-//       opts.title = 'Breaking News';
-//       opts.text = 'Have you met Ted?';
-//       opts.type = 'info';
-//       break;
-//     case 'success':
-//       opts.title = 'Good News Everyone';
-//       opts.text =
-//         "I've invented a device that bites shiny metal asses.";
-//       opts.type = 'success';
-//       break;
-//   }
-//   PNotify.alert(opts);
-// }
---
+++ src/js/specific.js
@@ -0,0 +1,13 @@
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+export default function specificQuery() {
+    error({
+        title: false,
+        text: 'Please enter a more specific query',
+        shadow: true,
+        stiker: false,
+        delay: 3000,
+    });
+}
